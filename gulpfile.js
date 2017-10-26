7d418316a6e5545e53f50bb2f4df5ac66f8ddf1b
Nerechka <sill@mail.ru>
2017-10-26 21:45:29 +0300

    Change scroll

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const browserSync = require('browser-sync');
 const autoprefixer = require('gulp-autoprefixer');
-
+const rename       = require('gulp-rename');
+const ejs          = require('gulp-ejs');
+const gutil        = require('gulp-util');
 // Автоперезагрузка при изменении файлов в папке `dist`:
 // Принцип: меняем файлы в `/src`, они обрабатываются и переносятся в `dist` и срабатывает автоперезагрузка.
 // Это таск нужен только при локальной разработке.
@@ -13,7 +15,6 @@ gulp.task('livereload', () => {
         server: {
             baseDir: 'dist'
         },
-        browser: 'google chrome',
         files: [
             'dist/**/*.*'
         ]
@@ -38,7 +39,9 @@ gulp.task('js', () => {
 });
 
 gulp.task('html', () => {
-    gulp.src('src/index.html')
+      gulp.src('src/index.ejs')
+    .pipe(ejs().on('error', gutil.log))
+    .pipe(rename('index.html'))
         .pipe(gulp.dest('./dist'));
 });
 
@@ -46,6 +49,7 @@ gulp.task('html', () => {
 gulp.task('watch', () => {
     gulp.watch('src/less/**/*.less', ['styles']);
     gulp.watch('src/**/*.html', ['html']);
+    gulp.watch('src/**/*.ejs', ['html']);
     gulp.watch('src/img/**/*.*', ['img']);
     gulp.watch('src/js/**/*.*', ['js']);
 });
